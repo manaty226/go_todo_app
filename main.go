568d5e41d1b135e7c5ea288e94f2dd24f5942290
manaty226 <ta.manaty0226@gmail.com>
2022-07-31 14:37:08 +0900

    fix: refactoring run function

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"golang.org/x/sync/errgroup"
+	// "golang.org/x/sync/errgroup"
 	"github.com/manaty226/go_todo_app/config"
 )
 
@@ -37,30 +35,8 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	s := &http.Server {
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-
-	eg, ctx := errgroup.WithContext(ctx)
-
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-				log.Printf("failed to close: %+v", err)
-				return err
-			}
-			return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
 
