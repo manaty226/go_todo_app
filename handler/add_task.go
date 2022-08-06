64c3eff289f99ea215ddd21c34719492073ad5b8
manaty226 <ta.manaty0226@gmail.com>
2022-08-06 11:12:50 +0900

    fix: fix handler but wip for test code

--- handler/add_task.go
+++ handler/add_task.go
@@ -3,15 +3,16 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/manaty226/go_todo_app/entity"
 	"github.com/manaty226/go_todo_app/store"
+	"github.com/jmoiron/sqlx"
 )
 
 type AddTask struct {
-	Store     *store.TaskStore
+	DB	*sqlx.DB
+	Repo	store.Repository
 	Validator *validator.Validate
 }
 
@@ -36,10 +37,9 @@ func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	t := &entity.Task{
 		Title:   b.Title,
-		Status:  entity.TaskStatusTodo,
-		Created: time.Now(),
+		Status:  entity.TaskStatusTodo,		
 	}
-	id, err := store.Tasks.Add(t)
+	at.Repo.AddTask(ctx, at.DB, t)
 	if err != nil {
 		RespondJSON(ctx, w, &ErrResponse{
 			Message: err.Error(),
@@ -48,6 +48,6 @@ func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	rsp := struct {
 		ID int `json:"id"`
-	}{ID: id}
+	}{ID: int(t.ID)}
 	RespondJSON(ctx, w, rsp, http.StatusOK)
 }
